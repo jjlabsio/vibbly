2008f26ccb0d6a4c7d22e4c27610e8500060d60a
송재진 <wowlxx28@gmail.com>
2025-10-17 17:13:00 +0900

    feat: add delete cron job

--- apps/web/app/api/cron/delete/route.ts
+++ apps/web/app/api/cron/delete/route.ts
@@ -1,3 +1,81 @@
+import { CommentStatus } from "@/generated/prisma";
+import prisma from "@/lib/prisma";
+import { getYouTubeClient } from "@/lib/youtube-account";
+
+type Result = {
+  success: boolean;
+  deletedIds: string[];
+  message?: string;
+};
+
 export async function GET() {
-  return new Response("hi");
+  const pendingDeleteComments = await prisma.comment.findMany({
+    where: {
+      status: CommentStatus.SpamPendingDelete,
+    },
+    select: {
+      id: true,
+      channelId: true,
+    },
+  });
+
+  const commentMap = new Map<string, string[]>();
+
+  pendingDeleteComments.forEach(({ id, channelId }) => {
+    if (!commentMap.has(channelId)) {
+      commentMap.set(channelId, []);
+    }
+
+    commentMap.get(channelId)!.push(id);
+  });
+
+  const channelIds = Array.from(commentMap.keys());
+
+  const result: Result[] = await Promise.all(
+    channelIds.map(async (channelId) => {
+      const commentIds = commentMap.get(channelId);
+      if (!commentIds) {
+        return {
+          success: false,
+          deletedIds: [],
+          message: "There is no pending comment id for this channel",
+        };
+      }
+
+      try {
+        await deleteProcess(channelId, commentIds);
+
+        return {
+          success: true,
+          deletedIds: commentIds,
+        };
+      } catch (error) {
+        console.error(error);
+        return {
+          success: false,
+          deletedIds: [],
+          message: `Delete comment failed for channel ${channelId} `,
+        };
+      }
+    })
+  );
+
+  return Response.json(result);
 }
+
+const deleteProcess = async (channelId: string, commentIds: string[]) => {
+  const channel = await prisma.youtubeAccount.findUnique({
+    where: {
+      channelId: channelId,
+    },
+  });
+  if (!channel) {
+    throw Error("There is no matched channel");
+  }
+
+  const client = await getYouTubeClient(channel);
+  await client.comments.setModerationStatus({
+    id: commentIds,
+    moderationStatus: "rejected",
+  });
+};
